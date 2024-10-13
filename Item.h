62b91bebba16f094fb1a17059979ad2d08be8b98
brklyn30 <brooklyndinh0908@gmail.com>
2024-10-13 21:33:51 +1030

    Update Item.h

--- Item.h
+++ Item.h
@@ -2,15 +2,21 @@
 #define ITEM_H
 
 #include <SFML/Graphics.hpp>
-#include <string>
+#include <memory>
+#include "BaseItem.h"
 
-struct Item {
+struct Item : public BaseItem {
     std::string name;
     int price;
     int stock;
     std::shared_ptr<sf::Texture> texture;
     sf::Sprite sprite;
     sf::Text quantityText;
+
+    // Override the virtual functions of BaseItem
+    std::string getName() const override { return name; }
+    int getPrice() const override { return price; }
+    sf::Sprite& getSprite() override { return sprite; }
 };
 
 #endif // ITEM_H
