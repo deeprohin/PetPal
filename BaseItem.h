a17f97ea2cfcf150a12babef29ccffffa6c43d9a
brklyn30 <brooklyndinh0908@gmail.com>
2024-10-13 21:34:19 +1030

    Update BaseItem.h

--- BaseItem.h
+++ BaseItem.h
@@ -1,4 +1,3 @@
-// BaseItem.h
 #ifndef BASEITEM_H
 #define BASEITEM_H
 
@@ -7,15 +6,11 @@
 
 class BaseItem {
 public:
-    virtual ~BaseItem() = default; // Virtual destructor for proper cleanup
+    virtual ~BaseItem() = default;
 
-    virtual std::string getName() const = 0; // Pure virtual method
-    virtual int getPrice() const = 0;         // Pure virtual method
-    virtual int getStock() const = 0;         // Pure virtual method
-    virtual void setStock(int newStock) = 0; // Pure virtual method
-    virtual void display() const = 0;         // Pure virtual method
-
-    // Additional methods as needed
+    virtual std::string getName() const = 0;
+    virtual int getPrice() const = 0;
+    virtual sf::Sprite& getSprite() = 0;
 };
 
 #endif // BASEITEM_H
